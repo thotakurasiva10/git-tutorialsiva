ca3ac7bd9f2f97ba9ec29341648dcd441d3bf7bf
thotakurasiva10 <thotakurasiva10@gmail.com>
2025-07-13 21:08:31 -0400

    Committing working directory files to local repo

---
+++ Example1.js
@@ -0,0 +1,11 @@
+let a = 5;
+let b = 10;
+
+console.log(`Before swap: a = ${a}, b = ${b}`);
+
+// Swapping using a temporary variable
+let temp = a;
+a = b;
+b = temp;
+
+console.log(`After swap: a = ${a},let a = 5; b = ${b}`);
